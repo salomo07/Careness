134bf48486569ab01e49cd927d55b2609f59b59a
salomo <sitompulsalomo@gmail.com>
2021-05-20 14:05:56 +0700

    Fixing SlideShow

--- src/component/SlideShow.js
+++ src/component/SlideShow.js
@@ -1,18 +1,18 @@
-import React,{useEffect} from 'react';
+import React,{useEffect,useCallback} from 'react';
 
 var $ = window.$;
 function SlideShow() {
     var eleDiv=React.createRef();
     useEffect(()=>{
-        if($('.slick-slider-responsive').length>0)
-        $(eleDiv.current).slick();
+        if($('.slick-initialized').length==0)
+        $('.slick-slider-responsive').slick();
     })
   return (
     <div className="col-sm-12 col-lg-12">
       <div className="mb-3 card" style={{borderRadius:'10px'}}>
             <div className="card-body">
             <h5 className="card-title">Responsive</h5>
-            <div className="slick-slider-responsive" ref={eleDiv}  data-slick='{"slidesToShow": 4, "slidesToScroll": 4}'>
+            <div className="slick-slider-responsive"ref={eleDiv}  data-slick='{"slidesToShow": 4, "slidesToScroll": 4}'>
                 <div>
                     <div className="slider-item">1</div>
                 </div>
